9b610152eb1f44bbae24aef1ce7ac9ddae5696d5
Alexandre <alexandrenicoud76@gmail.com>
2024-01-22 16:11:24 +0100

    suite

--- frontend/src/components/ModalConnexion.jsx
+++ frontend/src/components/ModalConnexion.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 function ModalConnexion() {
   const [openModal, setOpenModal] = useState(false);
-  const [email, setEmail] = useState('');
+  const [pseudo, setPseudo] = useState('');
 
   function onCloseModal() {
     setOpenModal(false);
@@ -17,41 +17,41 @@ function ModalConnexion() {
         <Modal.Header />
         <Modal.Body>
           <div className="space-y-6">
-            <h3 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+            <h3 className="text-xl font-medium text-gray-900 dark:text-white">Connecte toi</h3>
             <div>
               <div className="mb-2 block">
-                <Label htmlFor="email" value="Your email" />
+                <Label htmlFor="Pseudo" value="Pseudo" />
               </div>
               <TextInput
-                id="email"
-                placeholder="name@company.com"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
+                id="Pseudo"
+                placeholder="Ton pseudo"
+                value={pseudo}
+                onChange={(event) => setPseudo(event.target.value)}
                 required
               />
             </div>
             <div>
               <div className="mb-2 block">
-                <Label htmlFor="password" value="Your password" />
+                <Label htmlFor="password" value="Mot de passe" />
               </div>
               <TextInput id="password" type="password" required />
             </div>
             <div className="flex justify-between">
               <div className="flex items-center gap-2">
                 <Checkbox id="remember" />
-                <Label htmlFor="remember">Remember me</Label>
+                <Label htmlFor="remember">Se souvenir de moi</Label>
               </div>
               <a href="#" className="text-sm text-cyan-700 hover:underline dark:text-cyan-500">
-                Lost Password?
+                Mot de passe oublié ?
               </a>
             </div>
             <div className="w-full">
               <Button>Log in to your account</Button>
             </div>
             <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
-              Not registered?&nbsp;
+              Tu n'as pas de compte ?&nbsp;
               <a href="#" className="text-cyan-700 hover:underline dark:text-cyan-500">
-                Create account
+                Inscription
               </a>
             </div>
           </div>
